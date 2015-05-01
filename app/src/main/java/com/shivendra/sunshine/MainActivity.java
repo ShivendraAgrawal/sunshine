84318fe022118ccf07e5899c2a946309ffca9cb5
shivendra agrawal <shivendra.agrawal@gmail.com>
2015-05-01 09:50:03 +0530

    Add map and sharing capabilities

--- app/src/main/java/com/shivendra/sunshine/MainActivity.java
+++ app/src/main/java/com/shivendra/sunshine/MainActivity.java
@@ -1,13 +1,19 @@
 package com.shivendra.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +43,33 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-
             startActivity(new Intent( this, SettingsActivity.class));
             return true;
         }
 
+        if (id == R.id.action_map) {
+            showMap();
+            return true;
+        }
+
+
+
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showMap() {
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+        Uri geo_uri = Uri.parse("geo:0,0?q="+location);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geo_uri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+        }
+
     }
 }
